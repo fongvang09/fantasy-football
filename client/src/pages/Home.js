9b4a5435a169e2dc2a93d93e18730c35cc06dc78
Joseph Rhines <jprhines@gmail.com>
2021-03-11 18:59:01 -0600

    I deleted some default links in the navbar and took the search field out of the navbar. I placed the search field in a div on the Home.js. Iadded a divMain class in the index.css file and added a main div on the Home and Team js.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,9 +2,22 @@ import React from "react";
 
 const Home = () => {
     return (
-      <div>
-          <h1>Hello World</h1>
+        <>
+        <h1>Welcome To Fantasy-Football</h1>
+      <div className="container divMain">
+          
+          
+          
+          
       </div>
+      <br/>
+      <div className="container">
+      <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Find A Player" aria-label="Search"/>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+          </div>
+      </>
     );
   }
   
