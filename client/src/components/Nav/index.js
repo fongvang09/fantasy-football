9e361e2004c44168dc1522c679dae746a54c6f82
Joseph Rhines <jprhines@gmail.com>
2021-03-12 14:50:22 -0600

    I fixed the hamburger menu in mobile mode

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,28 +7,24 @@ function Nav() {
     
     
 <nav class="navbar navbar-expand-lg navbar-light ">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
             <Link className="navbar-brand" to="/">Home <span class="sr-only">(current)</span></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+          </li>
 
-            <Link className="navbar-brand" to="/team">Your Team <span class="sr-only">(current)</span></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+          <li><Link className="navbar-brand" to="/team">Your Team <span class="sr-only">(current)</span></Link></li>
+            
 
             {/* I ADDED LOGOUT BUTTON.. DO WE WANT ONE OF THESE YALL? */}
 
-            <Link className="navbar-brand" to="/">Log out<span class="sr-only">(current)</span></Link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+           <li><Link className="navbar-brand" to="/">Log out<span class="sr-only">(current)</span></Link></li>
             
             
-          </li>
+            
         </ul>
 
 
