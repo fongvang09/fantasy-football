86751c637d236d6258ad9c5fa139e5cc03aab88b
Evan Furniss <evanfurniss@gmail.com>
2021-03-12 14:33:33 -0600

    merging with incoming branch

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from'@auth0/auth0-react'
 import reportWebVitals from './reportWebVitals';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Auth0Provider
+      domain={domain}
+      clientId={clientID}
+      redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>,
   document.getElementById('root')
 );
 
--- server.js
+++ server.js
@@ -1,14 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const passport = require("passport");
-const facebookStrategy = require("passport-facebook").Strategy;
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-// Creates passport strategies for Google and Facebook
-
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
